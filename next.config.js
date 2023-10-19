c04fb1abed25b1f87bcfc0f9c40472e723a9055b
Λlex Jung <jjhyuk02@gmail.com>
2023-10-19 14:28:11 +0000

    Socket.IO Setup

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["uploadthing.com"],
   },
+  reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals.push({ bufferutil: "bufferutil", "utf-8-validate": "utf-8-validate", "supports-color": "supports-color" }); 
+    }
+    return config;
+  },
+};
